bf18ea241fde3eb62d73999e5b07b168f3073f08
wonkook wee <wonkookwee@wonkookui-MacBookAir.local>
2021-01-14 07:44:00 +0900

    express server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hi')
+const express = require('express');
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => {
+  console.log(`✅Listening on: http://localhost:${PORT}`)
+}
+
+app.listen(PORT, handleListening);
