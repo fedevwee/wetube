fa0026559d8cb1662e10317fad0118fd411fbc6e
wonkook wee <wonkookwee@wonkookui-MacBookAir.local>
2021-01-14 22:35:49 +0900

    handling routes with express

--- index.js
+++ index.js
@@ -7,4 +7,14 @@ const handleListening = () => {
   console.log(`✅Listening on: http://localhost:${PORT}`)
 }
 
+const handleHome = (req, res) => {
+  res.send('Hello from home!');
+}
+
+const handleProfile = (req, res) => {
+  res.send('You are on my Profile');
+}
+
+app.get('/', handleHome);
+app.get('/profile', handleProfile);
 app.listen(PORT, handleListening);
